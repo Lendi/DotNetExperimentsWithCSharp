22fef34b3fc0535d007cc1b812734c6c7ec27d96
Lendi <lendivihari@gmail.com>
2016-09-26 18:59:13 +0530

    Asset Management Datalayer
    Tables Creation, Datalayer skeleton

---
+++ AssetManagement/tables_creation.sql
@@ -0,0 +1,59 @@
+create database AssetManagement
+
+use AssetManagement
+
+create table users(
+emp_id int not null primary key,
+short_id varchar(25) not null unique,
+fname varchar(25) not null,
+lname varchar(25) not null,
+email_id varchar(25) not null,
+user_password varchar(25)not null,
+manager_id int not null,
+designation varchar(25),
+mobile int not null,
+date_of_join datetime,
+active varchar(1)
+)
+
+
+create table RequestInfo(
+request_id int not null primary key,
+emp_id int foreign key references users(emp_id),
+short_id varchar(25) not null unique,
+asset_name varchar(25),
+manager_id int not null,
+request_status int,
+request_date datetime, 
+manager_approved_date datetime, 
+remarks varchar(100)
+)
+
+create table AssetInfo(
+asset_id int not null primary key,
+assetname varchar(25) not null,
+emp_id int,
+request_id int,
+issueddate datetime, 
+)
+
+create table TransferHistory(
+transfer_id int, 
+from_emp_id int,
+manager_id int,
+asset_id int,
+assetname varchar(25),
+to_emp_id int,
+transferdate timestamp,
+transferstatus int,
+remarks varchar(100)
+)
+
+
+
+CREATE TABLE Orders
+(
+O_Id int NOT NULL PRIMARY KEY,
+OrderNo int NOT NULL,
+P_Id int FOREIGN KEY REFERENCES Persons(P_Id)
+)
