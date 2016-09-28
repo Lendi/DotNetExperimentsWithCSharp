807818cbce8b6a8a15266682061ad442c595d3e2
Lendi <lendivihari@gmail.com>
2016-09-28 17:36:26 +0530

    Js

---
+++ ASP.NET/HandsOnJavaScript/HandsOnJavaScript/Sample.js
@@ -0,0 +1,13 @@
+﻿function Greet(name) {
+    document.write("Hello" +name);
+}
+
+function Add(a, b) {
+    document.write(a+b);
+}
+
+function Square(a) {
+    var c = parseInt(a);
+    return c * c;
+}
+
