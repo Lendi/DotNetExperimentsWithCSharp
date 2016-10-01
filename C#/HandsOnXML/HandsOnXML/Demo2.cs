6871b41de9b0468e929fd1f02b816fb282ba0f6e
Lendi <lendivihari@gmail.com>
2016-10-01 15:40:43 +0530

    hands on xml

---
+++ C#/HandsOnXML/HandsOnXML/Demo2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace HandsOnXML
+{
+    class Demo2
+    {
+        static void Main()
+        {
+            string path = @"D:\DotNetExperimentsWithCSharp\C#\HandsOnXML\HandsOnXML\product.xml";
+            XmlDocument obj = new XmlDocument();
+            obj.Load(path);
+            //obj.Save(Console.Out);
+            //obj.Save("D:/text.xml");
+            //read product names
+            XmlNodeList list = obj.SelectNodes("products/product/pname");
+            foreach(XmlNode n in list ){
+                Console.WriteLine(n.InnerText);
+            }
+            Console.Clear();
+            //read all products
+            XmlNodeList list1 = obj.SelectNodes("products/product");
+            foreach (XmlNode n in list1) {
+                //Console.WriteLine(n.ChildNodes[0].InnerText);
+                Console.WriteLine(n.ChildNodes[0].InnerText);
+                //Console.WriteLine(n.ChildNodes[1].InnerText);
+            }
+        }
+    }
+}
---
+++ C#/HandsOnXML/HandsOnXML/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace HandsOnXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\DotNetExperimentsWithCSharp\C#\HandsOnXML\HandsOnXML\product.xml";
+            XmlReader obj = XmlReader.Create(path);//xml doesnt have public constructor, which isnt letting us to
+            //create objects
+            while (obj.Read())
+            {
+                Console.WriteLine(obj.Name);
+                if (obj.NodeType == XmlNodeType.Element) {
+                    obj.Read();
+                    Console.Write(obj.Value);
+                }
+            }
+        }
+    }
+}
