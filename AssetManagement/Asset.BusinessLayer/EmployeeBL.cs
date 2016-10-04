ae26ca4c0ee13cb62c12fdf77245cf285f49829c
Lendi <lendivihari@gmail.com>
2016-10-04 11:05:57 +0530

    --

---
+++ AssetManagement/Asset.BusinessLayer/EmployeeBL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using asset.datalayer;
+
+namespace Asset.BusinessLayer
+{
+     public class EmployeeBL
+    {
+         EmployeeDAO obj = new EmployeeDAO();
+
+         public void CreateRequest()
+        
+    }
+}
