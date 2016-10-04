9ed90af1d1f2ce29a6e1b1b7e5cc16067dd56a4c
Lendi <lendivihari@gmail.com>
2016-10-04 11:12:05 +0530

    --

---
+++ ASP.NET/UnitTestingDemo/UnitTestingDemo/Calculate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingDemo
+{
+    public class Calculate
+    {
+        public int Add(int a, int b) {
+            return a + b;
+
+        }
+
+        public int Mul(int a, int b) {
+            return a * b;
+        }
+    }
+}
